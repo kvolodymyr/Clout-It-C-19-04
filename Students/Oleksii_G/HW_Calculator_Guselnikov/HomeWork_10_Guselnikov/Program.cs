1cd596d163e94e40337805c3883dac4c0742bcfc
Algus88 <qa.oguselnikkov@gmail.com>
2019-12-23 18:46:38 +0200

    Guselnikov_Two_YjmeWorks
    HW for Races and Calculator

---
+++ Students/Oleksii_G/HW_Calculator_Guselnikov/HomeWork_10_Guselnikov/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HomeWork_10_Guselnikov
+{
+    class Program
+    {
+        private static readonly Regex operandRegex = new Regex(@"^(\d+(,\d|\.\d|\d*)\d*)$", RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+        private static readonly Regex operatorRegex = new Regex(@"^[\+\-\*\/]$", RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+        static void Main(string[] args)
+        {
+            var data = SetData();
+            switch (data.operation)
+            {
+                case ("+"):
+                    Console.WriteLine("Sum is " + Operations.Sum(data.firstOperand, data.secondOperand));
+                    break;
+                case ("-"):
+                    Console.WriteLine("Subtraction is " + Operations.Subtraction(data.firstOperand, data.secondOperand));
+                    break;
+                case ("*"):
+                    Console.WriteLine("Multiplication is " + Operations.Multiplication(data.firstOperand, data.secondOperand));
+                    break;
+                case ("/"):
+                    Console.WriteLine("Division is " + Operations.Division(data.firstOperand, data.secondOperand));
+                    break;
+                default:
+                    break;
+            }
+
+            /*  Console.WriteLine("Enter operation symbol");
+              var operation = Console.Read();
+              Operations
+              Console.WriteLine("Enter second operand");
+              var y = Console.Read();
+              Console.WriteLine("Sum is " + Operations.Sum(x, y));
+              Console.WriteLine("Substraction is " + Operations.Subtraction(x, y));
+              Console.WriteLine("Dividing is " + Operations.Division(x, y));
+              Console.WriteLine("Multiply is " + Operations.Multiplication(x, y));*/
+            Console.Read();
+        }
+
+        
+
+         static (double firstOperand, double secondOperand, string operation) SetData()
+            {
+            Console.WriteLine("Enter first operand");
+            var firstOperandRead = Console.ReadLine();
+            Console.WriteLine("Enter second operand");
+            var seconfOperandRead = Console.ReadLine();
+            if (!operandRegex.IsMatch(firstOperandRead) || !operandRegex.IsMatch(seconfOperandRead))
+            {
+                Console.WriteLine("data should be digits");
+                SetData();
+            }
+            if(firstOperandRead.Contains(","))
+            {
+                firstOperandRead = firstOperandRead.Replace(',', '.');
+            }
+            else if(seconfOperandRead.Contains(","))
+            {
+                seconfOperandRead = seconfOperandRead.Replace(',', '.');
+            }
+            var firstOperand = Double.Parse(firstOperandRead);
+            var secondOperand = Double.Parse(seconfOperandRead); 
+            Console.WriteLine("Enter operation");
+            var operation = Console.ReadLine();
+            // check regex
+            if (!operatorRegex.IsMatch(operation))
+            {
+                Console.WriteLine("Enter valid operator");
+                operation = SetData(firstOperand, secondOperand);
+            }
+            return (firstOperand, secondOperand, operation);
+        }
+
+        static string SetData(double firstOperand, double secondOperand)
+        {
+            Console.WriteLine("Enter operation");
+            var operation = Console.ReadLine();
+            // check regex
+            if (!operatorRegex.IsMatch(operation))
+            {
+                Console.WriteLine("Enter valid operator");
+                operation = SetData(firstOperand, secondOperand);
+            }           
+            return  operation;
+        }
+    }
+}
---
+++ Students/Oleksii_G/HW_Race_Guselnikov/RaceTasks/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ThreadTests;
+
+namespace RaceTasks
+{
+    class Program
+    {
+
+        static int averageSpeed;
+        static double startAcceliration;
+        static double sectionAcceliration;
+        static double distanceToFinish;
+
+        static List<BolidTaskVersion> list = new List<BolidTaskVersion> {
+               new BolidTaskVersion { CarPower = 700, MaxSpeed = 372, TankVolume = 60, TeamName = "Ferrari" },
+               new BolidTaskVersion { CarPower = 700, MaxSpeed = 372, TankVolume = 60, TeamName = "McLaren" },
+               new BolidTaskVersion { CarPower = 700, MaxSpeed = 372, TankVolume = 60, TeamName = "Fiat" },
+               new BolidTaskVersion { CarPower = 700, MaxSpeed = 372, TankVolume = 60, TeamName = "Reno" },
+            };
+
+        static void Main(string[] args)
+        {
+            Task[] tasks = new Task[list.Count];
+            BackTimerTaskVersion.RaceStart();
+            for (int i = 0; i < list.Count; i++)         
+            {
+                tasks[i] = new Task(()=>Race(list[i]));
+                tasks[i].Start();
+                Thread.Sleep(500);
+            }
+            Task.WaitAll(tasks);
+            Console.WriteLine("\nRace is completed");
+            Console.WriteLine("Race statistics:\n");
+            foreach (var item in list)
+            {
+                Console.WriteLine("Team is {3} Max speed is {0} km/h, Min speed is {1} km/h, Race time is {2} sec", item.MaxRaceSpeed, item.MinSpeed, item.RaceTime, item.TeamName);
+            }
+            Console.ReadLine();
+        }
+
+        public static void Race(BolidTaskVersion bolid)
+        {
+            Console.WriteLine("Team name {0}, Max speed {1} km/h, Power {2} h.p., Tank volume {3}", bolid.TeamName, bolid.MaxSpeed, bolid.CarPower, bolid.TankVolume);
+            DistanceMonitoringTaskVersion distanceMonitoring = new DistanceMonitoringTaskVersion();
+            averageSpeed = bolid.GetAverageSpeed();
+            bolid.RaceTime = 5;
+            startAcceliration = bolid.StartAcceleration(averageSpeed);
+            Console.Write("First start {0}, {1} m/s^2 ", bolid.TeamName, startAcceliration);
+            distanceToFinish = distanceMonitoring.PrintFirstCoveredDistance(startAcceliration);
+            while (distanceToFinish <= distanceMonitoring.FullDistance)
+            {
+                bolid.RaceTime++;
+                Console.WriteLine("");
+                Thread.Sleep(1000);
+                averageSpeed = bolid.GetMaxSectionSpeed();
+                sectionAcceliration = bolid.SectionAcceleration(averageSpeed);
+                Console.Write("{0}, {1} m/s^2, {2} m ", bolid.TeamName, sectionAcceliration, distanceToFinish);
+                distanceToFinish = distanceMonitoring.PrintCoverDistance(sectionAcceliration, distanceToFinish);
+            }
+        }
+    }
+}
