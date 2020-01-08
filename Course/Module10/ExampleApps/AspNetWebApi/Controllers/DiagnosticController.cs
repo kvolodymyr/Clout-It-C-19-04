2a9d336ce1eea3109e6569c55d3956676a838093
Vladimir <kvladimir2007@gmail.com>
2020-01-08 18:28:18 +0200

    example cookies

--- Course/Module10/ExampleApps/AspNetWebApi/Controllers/DiagnosticController.cs
+++ Course/Module10/ExampleApps/AspNetWebApi/Controllers/DiagnosticController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
+using System.Web;
 using System.Web.Http;
 
 namespace AspNetWebApi.Controllers
@@ -14,6 +15,9 @@ namespace AspNetWebApi.Controllers
         // http://localhost:11135/api/diagnostic/version
         [HttpGet]
         public string GetVersion() {
+            // CookieCollection
+            // HttpContext.Current.Request.Cookies.AllKeys
+            // HttpContext.Current.Response.Cookies.Add(new HttpCookie("key1", "djkhgjkhsdjkg"));
             Assembly assembly = this.GetType().Assembly;
             object[] attribs = assembly.GetCustomAttributes(typeof(AssemblyVersionAttribute), true);
             return attribs.Length > 0 ? ((AssemblyVersionAttribute)attribs[0]).Version : "unknown";
