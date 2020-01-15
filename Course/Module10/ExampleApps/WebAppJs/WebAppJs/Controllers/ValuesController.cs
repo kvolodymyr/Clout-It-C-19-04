92d49cd066f4fe3122e10e92aa215e922df104f8
Vladimir <kvladimir2007@gmail.com>
2020-01-15 21:31:58 +0200

    Web app + js

--- Course/Module10/ExampleApps/WebAppJs/WebAppJs/Controllers/ValuesController.cs
+++ Course/Module10/ExampleApps/WebAppJs/WebAppJs/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Serialization;
 using System.Web.Http;
 
 namespace WebAppJs.Controllers
@@ -18,13 +19,27 @@ namespace WebAppJs.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return $"value {id}";
+        }
+
+        [DataContract()]
+        public class MyData
+        {
+            [DataMember(Name = "fname")]
+            public string FirstName { get; set; }
+            [DataMember(Name = "lname")]
+            public string LastName { get; set; }
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post(MyData data)
         {
         }
+
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
